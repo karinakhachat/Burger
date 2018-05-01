202be9e6e42c1b332a89c85d0bcfe45a90f45123
Karina Khachatryan <karinakhachat@Karinas-MacBook-Air.local>
2018-05-01 07:46:50 -0400

    file structure created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jalapeno Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Burger", true);
